52447934b9ce60d3be1140429e67563aff8d1868
TEKURU-ZENO <132201660+TEKURU-ZENO@users.noreply.github.com>
2025-08-31 16:42:36 +0530

    Update Jobs.tsx

--- pages/Jobs.tsx
+++ pages/Jobs.tsx
@@ -3,7 +3,7 @@ import { DashboardHeader } from "../components/DashboardHeader";
 import { DashboardSidebar } from "../components/DashboardSidebar";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
-import { Card, CardTitle } from "../components/ui/card";
+import { Card, CardContent } from "../components/ui/card";
 import { Badge } from "../components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select";
