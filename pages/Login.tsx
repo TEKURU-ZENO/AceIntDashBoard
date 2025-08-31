8609d112f2234dc09ced612531c3db31dd552e6f
TEKURU-ZENO <132201660+TEKURU-ZENO@users.noreply.github.com>
2025-08-31 16:37:57 +0530

    Update Login.tsx

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -6,7 +6,6 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../co
 import { Checkbox } from "../components/ui/checkbox";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select";
 import { Alert, AlertDescription } from "../components/ui/alert";
-import { Badge } from "../components/ui/badge";
 import { Separator } from "../components/ui/separator";
 import { RouterContext } from "../App";
 import { useAuth } from "../components/AuthContext";
